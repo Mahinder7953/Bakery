d9887b5485c228a5a5f5b6745aea3155c360b618
Mahinder7953 <mahinderrathore2003@gmail.com>
2026-02-09 22:50:03 +0530

    Add Payment Service with initial structure

--- Cart-Service/src/main/java/com/example/Cart_Service/service/CartServiceImpl.java
+++ Cart-Service/src/main/java/com/example/Cart_Service/service/CartServiceImpl.java
@@ -3,15 +3,11 @@ package com.example.Cart_Service.service;
 import org.springframework.stereotype.Service;
 
 import com.example.Cart_Service.repository.CartItemRepository;
-import com.example.Cart_Service.repository.CartRepository;
-
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
 public class CartServiceImpl implements CartService {
-    private CartRepository cartRepository;
-
     private CartItemRepository cartItemRepository;
 
     @Override
